628936201fcbfc2673f3439d67319b298fc45c83
Adrastea966 <adrastea966@yahoo.com>
2024-03-28 17:46:35 -0300

    Inicio del proyecto

---
+++ src/componentes/Header.js
@@ -0,0 +1,14 @@
+//Header
+import '../App.css';
+
+function Header() {
+    return (
+        <>
+            <div className="header">
+                
+            </div>
+        </>
+    );
+}
+
+export default Header;
